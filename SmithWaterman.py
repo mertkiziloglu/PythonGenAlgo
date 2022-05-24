86cefa5458fb412f8af3efc28210b366f088b983
mert kiziloglu <mert_besiktas24@hotmail.com>
2022-05-24 09:03:52 +0300

    SmithWaterman.py

--- SmithWaterman.py
+++ SmithWaterman.py
@@ -1,118 +1,245 @@
-# Importing Python packages
-from enum import IntEnum
-import numpy as np
+#!/Users\Mert\PycharmProjects\test
 
 
-# Assigning the constants for the scores
-class Score(IntEnum):
-    MATCH = 1
-    MISMATCH = -1
-    GAP = -1
 
 
-# Assigning the constant values for the traceback
-class Trace(IntEnum):
-    STOP = 0
-    LEFT = 1
-    UP = 2
-    DIAGONAL = 3
+
+import argparse
+import os
+import re
+import sys
+import unittest
 
 
-# Reading the fasta file and keeping the formatted sequence's name and sequence
-def fasta_reader(sequence_file):
-    lines = open(sequence_file).readlines()
-    sequence_name_row = lines[0][1:]
-    sequence = lines[1]
-    return sequence_name_row.replace(" ", "").strip(), sequence.strip()
+
+match    = 2
+mismatch = -1
+gap      = -1
+seq1     = None
+seq2     = None
 
 
-# Implementing the Smith Waterman local alignment
-def smith_waterman(seq1, seq2):
-    # Generating the empty matrices for storing scores and tracing
-    row = len(seq1) + 1
-    col = len(seq2) + 1
-    matrix = np.zeros(shape=(row, col), dtype=np.int)
-    tracing_matrix = np.zeros(shape=(row, col), dtype=np.int)
+def main():
+    try:
+        parse_cmd_line()
+    except ValueError as err:
+        print('error:', err)
+        return
 
-    # Initialising the variables to find the highest scoring cell
-    max_score = -1
-    max_index = (-1, -1)
+    # The scoring matrix contains an extra row and column for the gap (-), hence
+    # the +1 here.
+    rows = len(seq1) + 1
+    cols = len(seq2) + 1
 
-    # Calculating the scores for all cells in the matrix
-    for i in range(1, row):
-        for j in range(1, col):
-            # Calculating the diagonal score (match score)
-            match_value = Score.MATCH if seq1[i - 1] == seq2[j - 1] else Score.MISMATCH
-            diagonal_score = matrix[i - 1, j - 1] + match_value
+    # Initialize the scoring matrix.
+    score_matrix, start_pos = create_score_matrix(rows, cols)
 
-            # Calculating the vertical gap score
-            vertical_score = matrix[i - 1, j] + Score.GAP
+    # Traceback. Find the optimal path through the scoring matrix. This path
+    # corresponds to the optimal local sequence alignment.
+    seq1_aligned, seq2_aligned = traceback(score_matrix, start_pos)
+    assert len(seq1_aligned) == len(seq2_aligned), 'aligned strings are not the same size'
 
-            # Calculating the horizontal gap score
-            horizontal_score = matrix[i, j - 1] + Score.GAP
-
-            # Taking the highest score
-            matrix[i, j] = max(0, diagonal_score, vertical_score, horizontal_score)
-
-            # Tracking where the cell's value is coming from
-            if matrix[i, j] == 0:
-                tracing_matrix[i, j] = Trace.STOP
-
-            elif matrix[i, j] == horizontal_score:
-                tracing_matrix[i, j] = Trace.LEFT
-
-            elif matrix[i, j] == vertical_score:
-                tracing_matrix[i, j] = Trace.UP
-
-            elif matrix[i, j] == diagonal_score:
-                tracing_matrix[i, j] = Trace.DIAGONAL
-
-                # Tracking the cell with the maximum score
-            if matrix[i, j] >= max_score:
-                max_index = (i, j)
-                max_score = matrix[i, j]
-
-    # Initialising the variables for tracing
-    aligned_seq1 = ""
-    aligned_seq2 = ""
-    current_aligned_seq1 = ""
-    current_aligned_seq2 = ""
-    (max_i, max_j) = max_index
-
-    # Tracing and computing the pathway with the local alignment
-    while tracing_matrix[max_i, max_j] != Trace.STOP:
-        if tracing_matrix[max_i, max_j] == Trace.DIAGONAL:
-            current_aligned_seq1 = seq1[max_i - 1]
-            current_aligned_seq2 = seq2[max_j - 1]
-            max_i = max_i - 1
-            max_j = max_j - 1
-
-        elif tracing_matrix[max_i, max_j] == Trace.UP:
-            current_aligned_seq1 = seq1[max_i - 1]
-            current_aligned_seq2 = '-'
-            max_i = max_i - 1
-
-        elif tracing_matrix[max_i, max_j] == Trace.LEFT:
-            current_aligned_seq1 = '-'
-            current_aligned_seq2 = seq2[max_j - 1]
-            max_j = max_j - 1
-
-        aligned_seq1 = aligned_seq1 + current_aligned_seq1
-        aligned_seq2 = aligned_seq2 + current_aligned_seq2
-
-    # Reversing the order of the sequences
-    aligned_seq1 = aligned_seq1[::-1]
-    aligned_seq2 = aligned_seq2[::-1]
-
-    return aligned_seq1, aligned_seq2
+    # Pretty print the results. The printing follows the format of BLAST results
+    # as closely as possible.
+    alignment_str, idents, gaps, mismatches = alignment_string(seq1_aligned, seq2_aligned)
+    alength = len(seq1_aligned)
+    print()
+    print(' Identities = {0}/{1} ({2:.1%}), Gaps = {3}/{4} ({5:.1%})'.format(idents,
+          alength, idents / alength, gaps, alength, gaps / alength))
+    print()
+    for i in range(0, alength, 60):
+        seq1_slice = seq1_aligned[i:i+60]
+        print('Query  {0:<4}  {1}  {2:<4}'.format(i + 1, seq1_slice, i + len(seq1_slice)))
+        print('             {0}'.format(alignment_str[i:i+60]))
+        seq2_slice = seq2_aligned[i:i+60]
+        print('Sbjct  {0:<4}  {1}  {2:<4}'.format(i + 1, seq2_slice, i + len(seq2_slice)))
+        print()
 
 
-if __name__ == "__main__":
-    # Reading the two required fasta sequences
-    file_1_name, file_1 = fasta_reader("Sequence1.fasta")
-    file_2_name, file_2 = fasta_reader("Sequence2.fasta")
+def parse_cmd_line():
+    '''Parse the command line arguments.
 
-    # Executing the Smith Waterman local alignment algorithm
-    output_1, output_2 = smith_waterman(file_1, file_2)
+    Create a help menu, take input from the command line, and validate the
+    input by ensuring it does not contain invalid characters (i.e. characters
+    that aren't the bases A, C, G, or T).
+    '''
 
-    print(file_1_name + ' ' + output_1 + '\n' + file_2_name + ' ' + output_2)
+
+
+    seq1 = "ATAGACGACATACAGACAGCATACAGACAGCATACAGA"
+    seq2 = "TTTAGCATGCGCATATCAGCAATACAGACAGATACG"
+
+
+def create_score_matrix(rows, cols):
+    '''Create a matrix of scores representing trial alignments of the two sequences.
+
+    Sequence alignment can be treated as a graph search problem. This function
+    creates a graph (2D matrix) of scores, which are based on trial alignments
+    of different base pairs. The path with the highest cummulative score is the
+    best alignment.
+    '''
+    score_matrix = [[0 for col in range(cols)] for row in range(rows)]
+
+    # Fill the scoring matrix.
+    max_score = 0
+    max_pos   = None    # The row and columbn of the highest score in matrix.
+    for i in range(1, rows):
+        for j in range(1, cols):
+            score = calc_score(score_matrix, i, j)
+            if score > max_score:
+                max_score = score
+                max_pos   = (i, j)
+
+            score_matrix[i][j] = score
+
+    assert max_pos is not None, 'the x, y position with the highest score was not found'
+
+    return score_matrix, max_pos
+
+
+def calc_score(matrix, x, y):
+    '''Calculate score for a given x, y position in the scoring matrix.
+
+    The score is based on the up, left, and upper-left neighbors.
+    '''
+    similarity = match if seq1[x - 1] == seq2[y - 1] else mismatch
+
+    diag_score = matrix[x - 1][y - 1] + similarity
+    up_score   = matrix[x - 1][y] + gap
+    left_score = matrix[x][y - 1] + gap
+
+    return max(0, diag_score, up_score, left_score)
+
+
+def traceback(score_matrix, start_pos):
+    '''Find the optimal path through the matrix.
+
+    This function traces a path from the bottom-right to the top-left corner of
+    the scoring matrix. Each move corresponds to a match, mismatch, or gap in one
+    or both of the sequences being aligned. Moves are determined by the score of
+    three adjacent squares: the upper square, the left square, and the diagonal
+    upper-left square.
+
+    WHAT EACH MOVE REPRESENTS
+        diagonal: match/mismatch
+        up:       gap in sequence 1
+        left:     gap in sequence 2
+    '''
+
+    END, DIAG, UP, LEFT = range(4)
+    aligned_seq1 = []
+    aligned_seq2 = []
+    x, y         = start_pos
+    move         = next_move(score_matrix, x, y)
+    while move != END:
+        if move == DIAG:
+            aligned_seq1.append(seq1[x - 1])
+            aligned_seq2.append(seq2[y - 1])
+            x -= 1
+            y -= 1
+        elif move == UP:
+            aligned_seq1.append(seq1[x - 1])
+            aligned_seq2.append('-')
+            x -= 1
+        else:
+            aligned_seq1.append('-')
+            aligned_seq2.append(seq2[y - 1])
+            y -= 1
+
+        move = next_move(score_matrix, x, y)
+
+    aligned_seq1.append(seq1[x - 1])
+    aligned_seq2.append(seq1[y - 1])
+
+    return ''.join(reversed(aligned_seq1)), ''.join(reversed(aligned_seq2))
+
+
+def next_move(score_matrix, x, y):
+    diag = score_matrix[x - 1][y - 1]
+    up   = score_matrix[x - 1][y]
+    left = score_matrix[x][y - 1]
+    if diag >= up and diag >= left:     # Tie goes to the DIAG move.
+        return 1 if diag != 0 else 0    # 1 signals a DIAG move. 0 signals the end.
+    elif up > diag and up >= left:      # Tie goes to UP move.
+        return 2 if up != 0 else 0      # UP move or end.
+    elif left > diag and left > up:
+        return 3 if left != 0 else 0    # LEFT move or end.
+    else:
+        # Execution should not reach here.
+        raise ValueError('invalid move during traceback')
+
+
+def alignment_string(aligned_seq1, aligned_seq2):
+    '''Construct a special string showing identities, gaps, and mismatches.
+
+    This string is printed between the two aligned sequences and shows the
+    identities (|), gaps (-), and mismatches (:). As the string is constructed,
+    it also counts number of identities, gaps, and mismatches and returns the
+    counts along with the alignment string.
+
+    AAGGATGCCTCAAATCGATCT-TTTTCTTGG-
+    ::||::::::||:|::::::: |:  :||:|   <-- alignment string
+    CTGGTACTTGCAGAGAAGGGGGTA--ATTTGG
+    '''
+    # Build the string as a list of characters to avoid costly string
+    # concatenation.
+    idents, gaps, mismatches = 0, 0, 0
+    alignment_string = []
+    for base1, base2 in zip(aligned_seq1, aligned_seq2):
+        if base1 == base2:
+            alignment_string.append('|')
+            idents += 1
+        elif '-' in (base1, base2):
+            alignment_string.append(' ')
+            gaps += 1
+        else:
+            alignment_string.append(':')
+            mismatches += 1
+
+    return ''.join(alignment_string), idents, gaps, mismatches
+
+
+def print_matrix(matrix):
+    '''Print the scoring matrix.
+
+    ex:
+    0   0   0   0   0   0
+    0   2   1   2   1   2
+    0   1   1   1   1   1
+    0   0   3   2   3   2
+    0   2   2   5   4   5
+    0   1   4   4   7   6
+    '''
+    for row in matrix:
+        for col in row:
+            print('{0:>4}'.format(col))
+        print()
+
+
+class ScoreMatrixTest(unittest.TestCase):
+
+    def test_matrix(self):
+
+        #                -   A   C   A   C   A   C   T   A
+        known_matrix = [[0,  0,  0,  0,  0,  0,  0,  0,  0],  # -
+                        [0,  2,  1,  2,  1,  2,  1,  0,  2],  # A
+                        [0,  1,  1,  1,  1,  1,  1,  0,  1],  # G
+                        [0,  0,  3,  2,  3,  2,  3,  2,  1],  # C
+                        [0,  2,  2,  5,  4,  5,  4,  3,  4],  # A
+                        [0,  1,  4,  4,  7,  6,  7,  6,  5],  # C
+                        [0,  2,  3,  6,  6,  9,  8,  7,  8],  # A
+                        [0,  1,  4,  5,  8,  8, 11, 10,  9],  # C
+                        [0,  2,  3,  6,  7, 10, 10, 10, 12]]  # A
+
+        global seq1, seq2
+        seq1 = 'AGCACACA'
+        seq2 = 'ACACACTA'
+        rows = len(seq1) + 1
+        cols = len(seq2) + 1
+
+        matrix_to_test, max_pos = create_score_matrix(rows, cols)
+        self.assertEqual(known_matrix, matrix_to_test)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
